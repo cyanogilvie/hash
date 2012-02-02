69ad253bc303d11b854a80100ec3b7096cdf3622
Cyan Ogilvie <cyan@tirion.(none)>
2012-02-02 11:16:51 +0200

    Added SHA-2 256, 384, 512, using BSD licensed implementation by Aaron D. Gifford - http://www.aarongifford.com/

--- generic/main.c
+++ generic/main.c
@@ -1,5 +1,6 @@
 #include <tclstuff.h>
 #include "md5.h"
+#include "sha2.h"
 
 static int glue_md5(cdata, interp, objc, objv) //<<<
 	ClientData		cdata;
@@ -92,15 +93,164 @@ static int glue_md5_finish(cdata, interp, objc, objv) //<<<
 }
 
 //>>>
+static int glue_sha2(cdata, interp, objc, objv) //<<<
+	ClientData		cdata;
+	Tcl_Interp*		interp;
+	int				objc;
+	Tcl_Obj *const	objv[];
+{
+	int				variant;
+	unsigned char*	data;
+	int				datalen;
+	Tcl_Obj*		res = NULL;
+
+	CHECK_ARGS(2, "variant data");
+
+	TEST_OK(Tcl_GetIntFromObj(interp, objv[1], &variant));
+	data = Tcl_GetByteArrayFromObj(objv[2], &datalen);
+
+	switch (variant) {
+		case 256:
+			{
+				SHA256_CTX		ctx;
+				char			out[SHA256_DIGEST_STRING_LENGTH];
+
+				SHA256_Init(&ctx);
+				SHA256_Update(&ctx, data, datalen);
+				SHA256_End(&ctx, out);
+
+				res = Tcl_NewStringObj(out, -1);
+			}
+			break;
+
+		case 384:
+			{
+				SHA384_CTX		ctx;
+				char			out[SHA384_DIGEST_STRING_LENGTH];
+
+				SHA384_Init(&ctx);
+				SHA384_Update(&ctx, data, datalen);
+				SHA384_End(&ctx, out);
+
+				res = Tcl_NewStringObj(out, -1);
+			}
+			break;
+
+		case 512:
+			{
+				SHA512_CTX		ctx;
+				char			out[SHA512_DIGEST_STRING_LENGTH];
+
+				SHA512_Init(&ctx);
+				SHA512_Update(&ctx, data, datalen);
+				SHA512_End(&ctx, out);
+
+				res = Tcl_NewStringObj(out, -1);
+			}
+			break;
+
+		default:
+			THROW_ERROR("Unsupported SHA-2 variant: ", Tcl_GetString(objv[1]));
+			break;
+	}
+
+	Tcl_SetObjResult(interp, res);
+
+	return TCL_OK;
+}
+
+//>>>
+static int glue_sha256(cdata, interp, objc, objv) //<<<
+	ClientData		cdata;
+	Tcl_Interp*		interp;
+	int				objc;
+	Tcl_Obj *const	objv[];
+{
+	unsigned char*	data;
+	int				datalen;
+	SHA256_CTX		ctx;
+	char			out[SHA256_DIGEST_STRING_LENGTH];
+
+	CHECK_ARGS(1, "data");
+
+	data = Tcl_GetByteArrayFromObj(objv[1], &datalen);
+
+	SHA256_Init(&ctx);
+	SHA256_Update(&ctx, data, datalen);
+	SHA256_End(&ctx, out);
+
+	Tcl_SetObjResult(interp, Tcl_NewStringObj(out, -1));
+
+	return TCL_OK;
+}
+
+//>>>
+static int glue_sha384(cdata, interp, objc, objv) //<<<
+	ClientData		cdata;
+	Tcl_Interp*		interp;
+	int				objc;
+	Tcl_Obj *const	objv[];
+{
+	unsigned char*	data;
+	int				datalen;
+	SHA384_CTX		ctx;
+	char			out[SHA384_DIGEST_STRING_LENGTH];
+
+	CHECK_ARGS(1, "data");
+
+	data = Tcl_GetByteArrayFromObj(objv[1], &datalen);
+
+	SHA384_Init(&ctx);
+	SHA384_Update(&ctx, data, datalen);
+	SHA384_End(&ctx, out);
+
+	Tcl_SetObjResult(interp, Tcl_NewStringObj(out, -1));
+
+	return TCL_OK;
+}
+
+//>>>
+static int glue_sha512(cdata, interp, objc, objv) //<<<
+	ClientData		cdata;
+	Tcl_Interp*		interp;
+	int				objc;
+	Tcl_Obj *const	objv[];
+{
+	unsigned char*	data;
+	int				datalen;
+	SHA512_CTX		ctx;
+	char			out[SHA512_DIGEST_STRING_LENGTH];
+
+	CHECK_ARGS(1, "data");
+
+	data = Tcl_GetByteArrayFromObj(objv[1], &datalen);
+
+	SHA512_Init(&ctx);
+	SHA512_Update(&ctx, data, datalen);
+	SHA512_End(&ctx, out);
+
+	Tcl_SetObjResult(interp, Tcl_NewStringObj(out, -1));
+
+	return TCL_OK;
+}
+
+//>>>
 int Hash_Init(Tcl_Interp* interp) //<<<
 {
 	if (Tcl_InitStubs(interp, "8.4", 0) == NULL)
 		return TCL_ERROR;
 
+	// MD5
 	NEW_CMD(PACKAGE_NAME "::md5", glue_md5);
 	NEW_CMD(PACKAGE_NAME "::md5_init", glue_md5_init);
 	NEW_CMD(PACKAGE_NAME "::md5_append", glue_md5_append);
 	NEW_CMD(PACKAGE_NAME "::md5_finish", glue_md5_finish);
+
+	// SHA-2
+	NEW_CMD(PACKAGE_NAME "::sha2", glue_sha2);
+	NEW_CMD(PACKAGE_NAME "::sha256", glue_sha256);
+	NEW_CMD(PACKAGE_NAME "::sha384", glue_sha384);
+	NEW_CMD(PACKAGE_NAME "::sha512", glue_sha512);
 
 	TEST_OK(Tcl_PkgProvide(interp, PACKAGE_NAME, PACKAGE_VERSION));
 
